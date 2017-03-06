5e6b8a739f045e87ce9c098f1c310f3bcbf06cc6
Niels Arts <niels.arts@hva.nl>
2017-03-06 15:53:00 +0100

    Templates geplaatst

---
+++ config/config.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * PHP example for PAD students.
+ * 
+ * @author Pieter Leek <p.d.leek@hva.nl>
+ * 
+ */
+//Constants to connect with the database
+define('DB_USERNAME', 'artsn001');
+define('DB_PASSWORD', '7L1fsVAUG5KngR'); // verander dit
+define('DB_HOST', 'oege.ie.hva.nl');
+define('DB_NAME', 'zartsn001');
+
