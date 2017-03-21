0b755ee379d0eb9aca7ce58b6100d2825740ac4e
Ruben van Rooijen <rooijer007@hva.nl>
2017-03-21 15:43:54 +0100

    if session redirect to template.php

--- index.php
+++ index.php
@@ -1,10 +1,12 @@
 <?php
+
 require_once 'google/config.php';
 session_start();
 if (!isset($_SESSION["user_id"]) && $_SESSION["user_id"] == "") {
-  header("location:" . SITE_URL . "view/signin.php");
-  exit();}
-
-  if (isset($_SESSION["user_id"]) && $_SESSION["user_id"] != "") {
-  header("location:" . SITE_URL . "view/dashboard.php");
-  exit();}
+    header("location:" . SITE_URL . "view/signin.php");
+    exit();
+} else {
+    header("location:" . SITE_URL . "view/template.php");
+    exit();
+}
+?>
